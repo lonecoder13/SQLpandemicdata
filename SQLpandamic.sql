9587f439ee63418e253d58e77b38b04b20fd59da
lonecoder13 <95169688+lonecoder13@users.noreply.github.com>
2022-07-04 20:08:42 +0530

    Update SQLpandamic.sql

--- SQLpandamic.sql
+++ SQLpandamic.sql
@@ -46,7 +46,17 @@ join PortfolioProject..CovidVaccinations$ vac
 	and dea.date=vac.date
 order by 1,2,3
 
+-- Creating View to store data for later visualizations
 
+Create View PercentPopulationVaccinated as
+Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations
+, SUM(CONVERT(int,vac.new_vaccinations)) OVER (Partition by dea.Location Order by dea.location, dea.Date) as RollingPeopleVaccinated
+--, (RollingPeopleVaccinated/population)*100
+From PortfolioProject..CovidDeaths dea
+Join PortfolioProject..CovidVaccinations vac
+	On dea.location = vac.location
+	and dea.date = vac.date
+where dea.continent is not null 
 
 
 
