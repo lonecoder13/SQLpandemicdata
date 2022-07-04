bb3e7d76f48ea1ed1b5c25922647e013cdc79de0
lonecoder13 <95169688+lonecoder13@users.noreply.github.com>
2022-07-04 20:06:25 +0530

    Add files via upload

---
+++ SQLpandamic.sql
@@ -0,0 +1,52 @@
+SELECT * 
+FROM PortfolioProject..CovidDeaths$
+select location ,date,total_cases,total_deaths,population
+from PortfolioProject.dbo.CovidDeaths$
+order by 1,2
+
+--TOTAL CASES vs TOTAL DEATHS--
+--showing likelihood of dying in my country
+select location ,date,total_cases,total_deaths,(total_deaths/total_cases)*100 AS DeathPercentage
+from PortfolioProject.dbo.CovidDeaths$
+where  location='India'
+order by DeathPercentage desc
+
+--countries with high percentage of infection
+select location,population,MAX(total_cases) AS hIGHEST_INFECTION_COUNT,MAX(total_cases/population)*100 AS INFECTED_POPULATION_PERCENTAGE
+from PortfolioProject..CovidDeaths$
+GROUP BY location,population
+ORDER BY INFECTED_POPULATION_PERCENTAGE DESC
+
+--COUNTRIES WITH HIGHEST DEATH COUNT PER POPULATION
+
+select location,population,MAX(total_deaths) AS total_death_COUNT,MAX(total_deaths/population)*100 AS death_POPULATION_PERCENTAGE
+from PortfolioProject..CovidDeaths$
+GROUP BY location,population
+ORDER BY 3 DESC
+
+
+--Global numbers
+select  Sum(new_cases) totalcases, Sum(cast(new_deaths as int)) as totaldeath  ,MAX(total_deaths/population)*100 AS death_POPULATION_PERCENTAGE
+from PortfolioProject..CovidDeaths$
+where continent is not null
+ORDER BY 1,2
+
+--
+select * 
+from PortfolioProject..CovidDeaths$ dea
+join PortfolioProject..CovidVaccinations$ vac
+	on dea.location=vac.location
+	and dea.date=vac.date
+---population vs vaccination
+select dea.location , dea.date,dea.population,vac.new_vaccinations  
+,sum(cast(vac.new_vaccinations as int)) over (Partition by dea.location Order by dea.location ,dea.date) as Total_vaccinated 
+from PortfolioProject..CovidDeaths$ dea
+join PortfolioProject..CovidVaccinations$ vac
+	on dea.location=vac.location
+	and dea.date=vac.date
+order by 1,2,3
+
+
+
+
+
